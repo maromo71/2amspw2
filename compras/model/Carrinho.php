5bd3486c8d910020e0048b9a421d3139133e3aa1
maromo71 <professormoraes@gmail.com>
2025-08-05 14:37:33 -0300

    Aula do dia 05 de agosto

--- compras/model/Carrinho.php
+++ compras/model/Carrinho.php
@@ -2,6 +2,24 @@
     //Carrinho.php -> Será um arquivo para o carrinho de compras.
     require_once 'Produto.php';
     class Carrinho {
+        //Array para armazenar os produtos no carrinho.
         private array $produtos = [];
     
+        //Método para adicionar um produto ao carrinho.
+        public function adicionarProduto(Produto $produto): void {
+            $this->produtos[] = $produto;
+        }
+
+        //Método para retornar os produtos do carrinho. (no carrinho)
+        public function getProdutos(): array {
+            return $this->produtos;
+        }
+        //Método para retornar o valor total do carrinho.
+        public function getTotal(): float {
+            $total = 0.0;
+            foreach ($this->produtos as $produto) {
+                $total += $produto->getPreco();
+            }
+            return $total;
+        }
     }
